dae807787333f9cdf4a373decbb4f70cadbbe182
Ben McKenzie <bmckenzie0522@gmail.com>
2020-08-18 13:40:29 -0400

    cowsays

---
+++ cowsays/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+from cowsays.models import Cowsays
+# Register your models here.
+admin.site.register(Cowsays)
---
+++ cowsays/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+
+class Cowsays(models.Model):
+    cowsays = models.TextField(max_length=80)
